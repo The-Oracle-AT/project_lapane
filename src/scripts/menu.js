f5f2d30a4da7a00c965f1b13729556e8287ca0cc
The-Oracle-AT <96382250+The-Oracle-AT@users.noreply.github.com>
2023-10-23 12:57:13 +0200

    Added view transitions, they are so easy to setup

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.addEventListener('astro:page-laod', () => { //make mobile menu interactive after navigating to a new page
+  document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
+})
